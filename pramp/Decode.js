c1636af097adc3aba6c550246f925e275eb4992a
kissinger chen <kchen@aurorasolartech.com>
2022-09-30 11:20:16 -0700

    use dp to handle the decode issue

--- pramp/Decode.js
+++ pramp/Decode.js
@@ -1,4 +1,4 @@
-let result = GetDecodeCount("1262");
+let result = GetDecodeDp("1213");
 console.log(result);
 
 function GetDecodeCount(input){
@@ -20,4 +20,30 @@ function GetDecodeCount(input){
     }
   }
   return additionalWay + 1;
+}
+
+function GetDecodeDp(input){
+  var memo = {};
+  return GetDecodeDpHelp(input, 0, memo);
+}
+
+function GetDecodeDpHelp(input, index, memo){
+  if(index == input.length - 1){
+    return 1;
+  }
+
+  if(memo[index]){
+    return memo[index];
+  }
+
+  var result = GetDecodeDpHelp(input, index + 1, memo);
+  if(input[index] == "1"){
+    result ++;
+  } else if(input[index] == "2"){
+    if(input[index + 1] <= "6"){
+      result ++;
+    }
+  } 
+  memo[index] = result;
+  return result;
 }
